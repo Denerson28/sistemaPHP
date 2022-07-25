3485f7f1e68d45af8ada5a4cb980c843c85b7ee7
Juliana Scapim <juscapim@id.uff.br>
2022-07-24 23:47:06 -0300

    inserindo css no administrador

--- App/Views/dashboardAdministrador/index.php
+++ App/Views/dashboardAdministrador/index.php
@@ -1,3 +1,10 @@
+<head>
+    <link href="<?= URL_CSS ?>login.css" rel="stylesheet">
+    <link href="<?= URL_CSS ?>dashboard.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
 <h1>Dashboard Administrador</h1>
 <?php
 if (isset($_SESSION['id']) && isset($_SESSION['nomeFuncionario'])) : ?>
@@ -5,8 +12,8 @@ if (isset($_SESSION['id']) && isset($_SESSION['nomeFuncionario'])) : ?>
     <div class="alert alert-success" role="alert">
         <h5>Administrador logado com sucesso</h5>
     </div>
-    <div class="row">
-        <div class="card mt-3 border-0">
+    <div class="row" id="space">
+        <div class="card mt-3 border-0" id ="tamanhobox">
             <div class="card-body px-2">
                 <i class="fas fa-user"></i> <strong>Nome</strong>
                 <p class="text-muted"><?= htmlentities(utf8_encode($_SESSION['nomeFuncionario'])) ?></p>
