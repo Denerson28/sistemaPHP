3e29eab4ff1e2b0fdf7b19525cca9a1c25ea82a7
denerson28 <denerson2808@hotmail.com>
2022-07-24 21:05:59 -0300

    alterando botão NOVO para vermelho

--- App/Views/cliente/index.php
+++ App/Views/cliente/index.php
@@ -1,5 +1,5 @@
 <!-- Button trigger modal -->
-<button type="button" id="btIncluir" class="btn btn-outline-primary mb-1">
+<button type="button" id="btIncluir" class="btn btn-danger mb-1">
     Novo
 </button>
 
